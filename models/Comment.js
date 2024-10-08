60b560749c9394bd43a4338de5099a0ce683f6d6
YigithanGumus <yigithangumuss@gmail.com>
2024-10-08 22:50:45 +0300

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const CommentSchema = new mongoose.Schema(
   {
-    post_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },  // Post'a referans
+    post_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
     email: { type: String, required: true },
     content: { type: String, required: true },
   },
