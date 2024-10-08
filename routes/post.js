ff16c6851427a91f84a8a1d12e42eefb1cd18cbc
YigithanGumus <yigithangumuss@gmail.com>
2024-10-08 22:50:25 +0300

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -6,6 +6,7 @@ const {
 
 const router = require("express").Router();
 const Post = require("../models/Post");
+const Category = require("../models/Category");
 const fs = require("fs");
 const path = require("path");
 const uploadFile = require("../utils/uploadFile");
@@ -70,16 +71,25 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
       path.join(__dirname, "/../images/")
     );
     
+
+    const findCategory = Category.findById({ _id:req.body.categories })
+
+    if (!findCategory) {
+      return res.status(500).json({ error: "Not found category" });
+    }
+    
     const newPost = new Post({
       title: req.body.title,
       content: req.body.content,
       img: imgPath,
       categories: req.body.categories, 
       article: req.body.article,
-    });
+    });  
 
+    
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
+    
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "File upload or post creation failed." });
@@ -90,8 +100,6 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const posts = await Post.find()
-      .populate('categories') // Kategorileri getirir
-      .populate('user_id');  // Kullanıcıyı getirir
     res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
@@ -100,9 +108,9 @@ router.get("/", async (req, res) => {
 
 router.get("/find/:id", async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id)
-      .populate('categories')
-      .populate('user_id');
+    const post = await Post.findById(req.params.id).
+                            populate("categories").exec();
+
     res.status(200).json(post);
   } catch (error) {
     res.status(404).json(error);
