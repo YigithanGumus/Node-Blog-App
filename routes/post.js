b9bf54c8c8beb5d3fb8fe6306694eb46d6c3857a
YigithanGumus <yigithangumuss@gmail.com>
2024-10-11 00:40:40 +0300

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -96,6 +96,14 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Post.findByIdAndDelete(req.params.id);
+    res.status(200).json("Post has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get("/", async (req, res) => {
   try {
