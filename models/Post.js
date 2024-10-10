b676a6bef232ac3ce85f97434bc05009fd8414c1
YigithanGumus <yigithangumuss@gmail.com>
2024-10-11 00:39:56 +0300

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -5,8 +5,8 @@ const PostSchema = new mongoose.Schema(
     title: { type: String, required: true },
     content: { type: String, required: true },
     img: { type: String, required: true },
-    categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }], 
-    tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }], 
+    categories: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' }, 
+    tags: { type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }, 
     article: { type: String, default: "Admin" },
   },
   { timestamps: true }
