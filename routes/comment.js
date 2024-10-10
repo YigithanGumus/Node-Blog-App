4d1b27b8751e797330a6c19c7bb4e54923f4eae1
YigithanGumus <yigithangumuss@gmail.com>
2024-10-11 00:42:29 +0300

    Update comment.js

--- routes/comment.js
+++ routes/comment.js
@@ -20,24 +20,47 @@ router.post("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
   try {
-    const comments = await Comment.find().populate('post_id'); // Post bilgilerini getirir
+    const comments = await Comment.find().populate('post_id');
+
+    if (!comments) {
+      return res.status(404).json({ error: "Comments not found." });
+    }
+
     res.status(200).json(comments);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    category = await Comment.findByIdAndDelete(req.params.id);
+    
+    if (!category) {
+      return res.status(404).json({ error: "Comment not found." });
+    }
+
+    res.status(200).json("Comment has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get("/find/:id", async (req, res) => {
   try {
     const Comment = await Comment.findById(req.params.id).populate('post_id');
+
+    if (!Comment) {
+      return res.status(404).json({ error: "Comment not found." });
+    }
+
     res.status(200).json(Comment);
   } catch (error) {
     res.status(404).json(error);
   }
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     await Comment.findByIdAndDelete(req.params.id);
     res.status(200).json("Comment has been deleted");
